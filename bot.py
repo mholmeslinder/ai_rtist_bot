8d639e2efe6a3f7b43cb04acd923f61015a3a3c9
Matt Linder <mholmeslinder@gmail.com>
2020-03-03 15:45:42 -0800

    refactoring for production

--- bot.py
+++ bot.py
@@ -7,23 +7,23 @@ import generate_artist
 import os
 
 # use this one for testing
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
-CONSUMER_KEY = os.getenv('CONSUMER_KEY')
-CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
-ACCESS_KEY = os.getenv('ACCESS_KEY')
-ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+# CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+# CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+# ACCESS_KEY = os.getenv('ACCESS_KEY')
+# ACCESS_SECRET = os.getenv('ACCESS_SECRET')
 
 # use this for production; set vars in heroku dashboard
-# from os import environ
-# CONSUMER_KEY = environ['CONSUMER_KEY']
-# CONSUMER_SECRET = environ['CONSUMER_SECRET']
-# ACCESS_KEY = environ['ACCESS_KEY']
-# ACCESS_SECRET = environ['ACCESS_SECRET']
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
-# INTERVAL = 60 * 60 * 6  # tweet every 6 hours
-INTERVAL = 15  # every 15 seconds, for testing
+INTERVAL = 60 * 60 * 6  # tweet every 6 hours
+# INTERVAL = 15  # every 15 seconds, for testing
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
